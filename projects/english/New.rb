25b79c4e7fd1f26937fd3b4f053fd47068af05dc
Lenna Hammer <lennahammer@gmail.com>
2016-05-24 23:14:49 +0800

    fix

--- projects/english/New.rb
+++ projects/english/New.rb
@@ -7,6 +7,9 @@ module Enumerable
   def stable_sort_by
     sort_by.with_index { |x, idx| [yield(x), idx] }
   end
+  def stable_sort_by
+    self.map.with_index{|x,i|[x,i]}.sort.map(&:first)
+  end
 end
 
 $ab = open('GRE红宝书.txt','r:gbk:utf-8').each_line.map{|line|line.tap(&:chomp!).split("\t")}.inject({}){|h,(k,v)|h[k]=v;h};
@@ -37,34 +40,20 @@ $mhyph = {}&&open('mhyph.txt','r:MacRoman:utf-8').each_line.inject({}){|a,b|b.ch
 
 
 $text = $group.keys.inject({}){|a,key|
-  a[key[/\d+.*$/]] = "**** #{key}\n\n"<<($group[key].map{|w| gg(w)<<""}.stable_sort_by{|x|x[/ (.\.)/]||''}*"\n")
+  a[key[/\d+.*$/]] = "***** #{key}\n\n"<<($group[key].map{|w| gg(w)<<""}.stable_sort_by{|x|x[/ (.\.)/]||''}*"\n")
   a
 }
 
 p $text.first
-#gets
-#out = open("output_6000_tree.txt","w:utf-8")
+
 open("output_6000_tree.txt","w:utf-8"){|out|
-#  p open('outline.txt','r:utf-8').read.gsub(/^\*\*\* (.+)\. .*$/){|x|
-#    "*** #{$text[$~[1]]}"
-##    #$~[1]
-##    p $text[$~[1]].encoding;gets
-##    "*** #{$~[1]}"
-#  }
-   out.puts(open('outline.txt','r:utf-8').read.gsub("\n","\n\n").gsub(/^\*\*\*\* (.+)\. .*$/){|x|
-     "#{$text[$~[1]]}"
-   })
+  out.puts "* GRE Words / Roget's Thesaurus\n\n\n"
+
+  out.puts(open('outline.txt','r:utf-8').read.gsub("\n","\n\n").gsub(/^\*\*\*\*\* (.+)\. .*$/){|x|
+    "#{$text[$~[1]]||"#{$~[0].gsub(/(\d\S+)\. (\S+)/,"\\2 \\1")}"}"
+  })
   out.puts "\n* \n"
   out.puts ($ab.keys-$my_keys).map{|w| gg(w)}
 }
 
-#out.close
-
-#$group.keys.sort_by{|x|x[/\d+/].to_i}.each{|key|
-#  out.puts "\n* #{key}\n===================="
-#  out.puts $group[key].map{|w| gg(w)}.stable_sort_by{|x|x[/ (.\.)/]||''}
-#}
-#out.puts "\n*\n========================"
-#out.puts ($ab.keys-$my_keys).map{|w| gg(w)}
-#out.close
 p :ok
